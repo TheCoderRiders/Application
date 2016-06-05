d41b7af6f98ae21df86767c92ae850d21b31a99b
unknown <harshal.s@PF08ETP5.ezdi.local>
2016-06-05 20:09:59 +0530

    added comments in code,middle list UI and file details UI added

--- ZingatApp/src/main/webapp/WEB-INF/CoderCode/app/login/loginCtrl.js
+++ ZingatApp/src/main/webapp/WEB-INF/CoderCode/app/login/loginCtrl.js
@@ -1,8 +1,16 @@
 angular.module('LoginController', [])
-    .controller("loginCtrl",["$scope","$location", function($scope,$location){
+    .controller("loginCtrl",["$scope","$location","$http", function($scope,$location,$http){
         $scope.processLogin = function(){
             var userName = $scope.userName;
             var userPassword = $scope.userPassword;
+            
+            /*$http.get('extras/data.json').success(function(response){ //make a get request to mock json file.
+                $scope.data = response; //Assign data received to $scope.data
+            })
+            .error(function(err){
+                //handle error
+            })*/
+            
             if(userName == 'a' && userPassword == 'a'){
                 $location.path('/landingPage');
             }
