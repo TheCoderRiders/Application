1a1a3df030e128e7d2a12d168a18429bb34b2ac9
akash.p <akash.p@ezdi.us>
2016-06-08 09:50:49 +0530

    login ui controller

--- ZingatApp/src/main/resources/static/app/login/loginCtrl.js
+++ ZingatApp/src/main/resources/static/app/login/loginCtrl.js
@@ -1,18 +1,33 @@
 angular.module('LoginController', [])
     .controller("loginCtrl",["$scope","$location","$http", function($scope,$location,$http){
         $scope.processLogin = function(){
-            var userName = $scope.userName;
-            var userPassword = $scope.userPassword;
-            
-            /*$http.get('extras/data.json').success(function(response){ //make a get request to mock json file.
-                $scope.data = response; //Assign data received to $scope.data
+            var credentials=new Object();
+            credentials.userName = $scope.userName;
+            credentials.userPassword = $scope.userPassword;
+
+            var headers = credentials ? {authorization : "Basic "
+                + btoa(credentials.userName + ":" + credentials.userPassword)
+            } : {};
+
+            $http.get('user',{headers : headers}, {
+                headers: { 'Content-Type': 'application/json',
+                    'Accept': 'application/json'}
+            }).success(function(data){ //make a get request to mock json file.
+                //$scope.data = response; //Assign data received to $scope.data
+                if (data.name) {
+                    $scope.authenticated = true;
+                    $location.path('/landingPage');
+                } else {
+                    $scope.authenticated = false;
+                }
             })
-            .error(function(err){
-                //handle error
-            })*/
-            
-            if(userName == 'a' && userPassword == 'a'){
-                $location.path('/landingPage');
-            }
+                .error(function(err){
+                    console.log(err);
+                    $scope.authenticated = false;
+                })
+
+            /* if(userName == 'a' && userPassword == 'a'){
+             $location.path('/landingPage');
+             }*/
         }
     }]);
