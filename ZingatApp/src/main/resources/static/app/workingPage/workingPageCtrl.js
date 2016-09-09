6cfa677d14d8ecce6a524ba4708ebdcfbf718996
unknown <harshal.s@PF08ETP5.ezdi.local>
2016-09-09 20:56:07 +0530

    working page controller changes

--- ZingatApp/src/main/resources/static/app/workingPage/workingPageCtrl.js
+++ ZingatApp/src/main/resources/static/app/workingPage/workingPageCtrl.js
@@ -24,12 +24,69 @@ angular.module('WorkingPageController', [])
         });
 
         $scope.codeStatus = function(code,section,actionName){
-            console.log(actionName);
-            console.log(code);
-            console.log(section);
-            var temp = $scope.acceptedCode;
-            
+            var selectedCode = {};
+            selectedCode.sectionName = section.sectionName;
+            selectedCode.codes = [];
+            delete code.$$hashKey;
+            selectedCode.codes.push(code);
 
+            
+            if($(event.target).parent().parent().parent().parent().children('div').length == 1){
+                $(event.target).parent().parent().parent().parent().remove();
+            }
+            $(event.target).parent().parent().parent().remove();
+            if($(event.target).attr('id') == "on"){
+                var temp = $scope.acceptedCode;
+                var codeLength;
+                var temp2 = [];
+                var dummyElement;
+                var i = 0;
+                if(temp){
+                    codeLength = temp.length;
+                    for(i=0; i<codeLength; i++){
+                        if(temp[i].sectionName == selectedCode.sectionName){
+                            dummyElement = selectedCode;
+                            break;
+                        }
+                    }
+                    /*to check for existing element*/
+                    if(i == codeLength){
+                        temp.push(selectedCode);
+                    }else{
+                        temp[i].codes.push(selectedCode.codes[0]);
+                    }
+                }else{
+                    codeLength = 1;
+                    temp = [];
+                    temp.push(selectedCode);
+                }
+                $scope.acceptedCode = temp;
+            }else{
+                var temp = $scope.rejectedCode;
+                var codeLength;
+                var temp2 = [];
+                var dummyElement;
+                var i = 0;
+                if(temp){
+                    codeLength = temp.length;
+                    for(i=0; i<codeLength; i++){
+                        if(temp[i].sectionName == selectedCode.sectionName){
+                            dummyElement = selectedCode;
+                            break;
+                        }
+                    }
+                    /*to check for existing element*/
+                    if(i == codeLength){
+                        temp.push(selectedCode);
+                    }else{
+                        temp[i].codes.push(selectedCode.codes[0]);
+                    }
+                }else{
+                    codeLength = 1;
+                    temp = [];
+                    temp.push(selectedCode);
+                }
+                $scope.rejectedCode = temp;
+            }
         }
-        
     }]);
