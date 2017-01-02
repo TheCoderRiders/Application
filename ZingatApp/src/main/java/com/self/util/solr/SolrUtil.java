a9f64f9e0c963340e740223f10611607f43c1a33
akash.p <akash.p@ezdi.us>
2017-01-02 15:43:40 +0530

    Solr Authentication

--- ZingatApp/src/main/java/com/self/util/solr/SolrUtil.java
+++ ZingatApp/src/main/java/com/self/util/solr/SolrUtil.java
@@ -1,7 +1,10 @@
 package com.self.util.solr;
 
+import org.apache.http.client.HttpClient;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpClientUtil;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.params.ModifiableSolrParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,21 @@ public class SolrUtil {
 
     @Bean
     public SolrServer codeSearchSolrServer(@Value("${solr.server}") String solrServerUrl,
-                                     @Value("${solr.core.icd.code.search}") String solrCodeSearchCore){
-        return new HttpSolrServer(solrServerUrl+"/"+solrCodeSearchCore);
+                                           @Value("${solr.core.icd.code.search}") String solrCodeSearchCore,
+                                           @Value("${solr.connection.max}") Integer maxConnection,
+                                           @Value("${solr.connection.max.per.host}") Integer maxConnectionPerHost,
+                                           @Value("${solr.connection.follow.redirects}") Boolean followRedirects,
+                                           @Value("${solr.connection.user.name}") String userName,
+                                           @Value("${solr.connection.user.password}") String password){
+
+        ModifiableSolrParams params = new ModifiableSolrParams();
+        params.set(HttpClientUtil.PROP_MAX_CONNECTIONS, maxConnection);
+        params.set(HttpClientUtil.PROP_MAX_CONNECTIONS_PER_HOST, maxConnectionPerHost);
+        params.set(HttpClientUtil.PROP_FOLLOW_REDIRECTS, followRedirects);
+        params.set(HttpClientUtil.PROP_BASIC_AUTH_USER, userName);
+        params.set(HttpClientUtil.PROP_BASIC_AUTH_PASS, password);
+        //HttpClientUtil.addRequestInterceptor(new PreemptiveAuthInterceptor());
+        HttpClient httpClient = HttpClientUtil.createClient(params);
+        return new HttpSolrServer(solrServerUrl+"/"+solrCodeSearchCore,httpClient);
     }
 }
